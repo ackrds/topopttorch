e2f9de73415c0c91412a01e0920889597bbabc20
Ali Cem <ack@Alis-MacBook-Pro.local>
2023-07-21 18:57:11 +0300

    finished until the sK part. time for the sparse tensors

--- topopttorch.py
+++ topopttorch.py
@@ -48,8 +48,14 @@ def top3d(nelx, nely, nelz, volfrac, penal, rmin):
     nodeids = nodeids.repeat(1, nodeidz.shape[0]) + nodeidz.repeat(nodeids.shape[0], 1)
     edofVec = 3 * nodeids + 1
     
-    offsets = torch.tensor([0, 1, 2, 3 * nely + torch.tensor([3, 4, 5, 0, 1, 2]), -3, -2, -1, 3 * (nely + 1) * (nelx + 1) + torch.tensor([0, 1, 2, 3 * nely + torch.tensor([3, 4, 5, 0, 1, 2]), -3, -2, -1])])
-    edofMat = edofVec.unsqueeze(1) + offsets.unsqueeze(0)  # Perform element-wise addition with broadcasting
+    offset_tensors = [torch.Tensor([0, 1, 2]), 3*nely + torch.Tensor([3, 4, 5, 0, 1, 2]), 
+                      torch.Tensor([-3, -2, -1]), 3 * (nely + 1) * (nelx + 1) + torch.tensor([0, 1, 2]),
+                      (3 * (nely + 1) * (nelx + 1)+ 3 * nely) + torch.tensor([3, 4, 5, 0, 1, 2]),
+                      3 * (nely + 1) * (nelx + 1) + torch.Tensor([-3, -2, -1])]
+    offset_tensors = torch.hstack(offset_tensors)
+    offset_tensors = offset_tensors.repeat(nele, 1)
+    edofVec = edofVec.flatten().view(-1, 1).repeat(1, 24)
+    edofMat = edofVec + offset_tensors  # Perform element-wise addition with broadcasting
     
     # Reshape iK and jK
     edofMat_flat = edofMat.flatten()
@@ -57,43 +63,34 @@ def top3d(nelx, nely, nelz, volfrac, penal, rmin):
     jK = torch.reshape(torch.kron(edofMat_flat, torch.ones(1, 24)), (24 * 24 * nele,))
     
     # PREPARE FILTER
-    rmin = torch.ceil(rmin)  # Assuming rmin is already defined
-    iH = torch.ones(nele * (2 * (rmin.long() - 1) + 1) ** 2)
-    jH = torch.ones_like(iH)
-    sH = torch.zeros_like(iH)
-    k = 0
-    
-
+    rmin = torch.Tensor([rmin]) # Assuming rmin is already defined
     nele = nelx * nely * nelz
-    iH = []
-    jH = []
-    sH = []
+    size = nele * (2 * int(torch.ceil(rmin)) - 1) ** 2
+
+    iH = torch.ones(size, dtype=torch.int64)
+    jH = torch.ones(size, dtype=torch.int64)
+    sH = torch.zeros(size)
+
     k = 0
-    
     for k1 in range(1, nelz + 1):
         for i1 in range(1, nelx + 1):
             for j1 in range(1, nely + 1):
                 e1 = (k1 - 1) * nelx * nely + (i1 - 1) * nely + j1
-                k1_min = max(k1 - (torch.ceil(rmin).long() - 1), 1)
-                k1_max = min(k1 + (torch.ceil(rmin).long() - 1), nelz)
-                i1_min = max(i1 - (torch.ceil(rmin).long() - 1), 1)
-                i1_max = min(i1 + (torch.ceil(rmin).long() - 1), nelx)
-                j1_min = max(j1 - (torch.ceil(rmin).long() - 1), 1)
-                j1_max = min(j1 + (torch.ceil(rmin).long() - 1), nely)
-    
-                for k2 in range(k1_min, k1_max + 1):
-                    for i2 in range(i1_min, i1_max + 1):
-                        for j2 in range(j1_min, j1_max + 1):
-                            e2 = (k2 - 1) * nelx * nely + (i2 - 1) * nely + j2
-                            k += 1
-                            iH.append(e1)
-                            jH.append(e2)
-                            sH.append(max(0, rmin - torch.sqrt((i1 - i2) ** 2 + (j1 - j2) ** 2 + (k1 - k2) ** 2)))
-    
+                for k2 in range(max(k1 - (int(torch.ceil(rmin)) - 1), 1), min(k1 + (int(torch.ceil(rmin)) - 1), nelz) + 1):
+                    for i2 in range(max(i1 - (int(torch.ceil(rmin)) - 1), 1), min(i1 + (int(torch.ceil(rmin)) - 1), nelx) + 1):
+                        for j2 in range(max(j1 - (int(torch.ceil(rmin)) - 1), 1), min(j1 + (int(torch.ceil(rmin)) - 1), nely) + 1):
+                            try:
+                                e2 = (k2 - 1) * nelx * nely + (i2 - 1) * nely + j2
+                                k = k + 1
+                                iH[k - 1] = e1
+                                jH[k - 1] = e2
+                                sH[k - 1] = max(0, rmin - torch.sqrt(torch.tensor((i1 - i2) ** 2 + (j1 - j2) ** 2 + (k1 - k2) ** 2, dtype=torch.float32)))
+                            except IndexError:
+                                break
     iH = torch.tensor(iH)
     jH = torch.tensor(jH)
     sH = torch.tensor(sH)
-    
+
     H = torch.sparse_coo_tensor(torch.stack([iH, jH]), sH, (nele, nele))
     Hs = torch.sum(H, dim=1)
 
@@ -105,7 +102,8 @@ def top3d(nelx, nely, nelz, volfrac, penal, rmin):
     while change > tolx and loop < maxloop:
         loop += 1
         # FE-ANALYSIS
-        sK = torch.reshape(KE.flatten() * (Emin + xPhys.flatten() ** penal * (E0 - Emin)), (24 * 24 * nele, 1))
+        sK = torch.reshape(KE.flatten().unsqueeze(-1) @ (Emin + xPhys.flatten() ** penal * (E0 - Emin)).unsqueeze(0), (24 * 24 * nele, 1))
+        print(sK.shape)
         K = torch.sparse_coo_tensor(torch.stack([iK, jK]), sK, (24 * nele, 24 * nele)).to_dense()
         K = (K + K.t()) / 2
         U = torch.zeros_like(F)
@@ -198,17 +196,16 @@ def lk_H8(nu):
                    [k[9],  k[8],  k[2],  k[7],  k[13], k[7]],
                    [k[11], k[2],  k[9],  k[10], k[7],  k[13]]]).squeeze()
     
-    print(K1.shape,  K2.shape, K3.shape, K4.shape, K5.shape, K6.shape)
     # Assemble the global stiffness matrix KE
     KE = (1 / ((nu + 1) * (1 - 2 * nu))) * np.block([[K1, K2, K3, K4],
                                                      [K2.T, K5, K6, K3.T],
                                                      [K3.T, K6, K5.T, K2.T],
                                                      [K4, K3, K2, K1]])
 
-    return KE
+    return torch.Tensor(KE)
 
 # Example usage:
     return KE
 
 if __name__ == "__main__":
-    top3d(20,20,20,0.4, 3.0, 1.5)
+    top3d(2,2,2,0.4, 3.0, 1.5)
